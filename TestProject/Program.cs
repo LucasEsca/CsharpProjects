0691ad8d19aa22f873e998ba2e9337ad97e5be31
Fair Lucas <lukitaa.27@gmail.com>
2024-03-27 11:56:05 -0300

    commit2

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,29 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+﻿string permission = "Admin|Manager";
+int level = 53;
 
-Console.WriteLine(largerValue);
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
